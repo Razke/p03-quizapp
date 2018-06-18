f78524dbe132bf2fb56481ffa99115cef8de0023
razscu <razvan.dinculescu@outlook.com>
2018-06-19 01:02:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+#### Table of Contents
+  1. [About](#about)
+  2. [Resources used in this project](#resources-used-in-this-project)
+  3. [Reflections](#reflections)
+
+  # About
+  Objective: design and implement a short quiz app about some topic you are familiar with.
+
+<img src="https://i.imgur.com/VdSG7bn.png" width="310" height="530">
+
+## Resources used in this project
+
+* Placeholders
+
+* Placeholders
+
+* [What should be in my .gitignore for an Android Studio project](https://stackoverflow.com/a/17803964/8651044)
+
+* [Common Android Views Cheat Sheet](https://drive.google.com/file/d/0B5XIkMkayHgRMVljUVIyZzNmQUU/view)
+
+* [Layout Cheat Sheet](https://s3.amazonaws.com/video.udacity-data.com/topher/2016/June/576abcfc_layout-cheat-sheet/layout-cheat-sheet.pdf)
+
+* [Image Asset Studio](https://developer.android.com/studio/write/image-asset-studio.html)
+
+* [How to disable action bar permanently](https://stackoverflow.com/a/44754842/8651044)
+
+* [How to disable keypad popup](https://stackoverflow.com/questions/10611833/how-to-disable-keypad-popup-when-on-edittext/13908440#13908440)
+
+* [About JavaDoc](https://stackoverflow.com/questions/19172015/what-exactly-is-javadoc/19172263#19172263)
+
+* [Java Operators](http://www.mathcs.emory.edu/~cheung/Courses/170/Syllabus/04/shorthand.html)
+
+* [Mentor review]()
+
+* Android Studio Plugins: CodeGlance, Key promoter, Markdown Navigator, Material Theme UI
+
+## Reflections
+
+* Placeholder
+
+[Back to Top](#table-of-contents)
---
+++ app/src/main/java/com/example/raz/p03_quizapp/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.raz.p03_quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+/**
+ * This app displays questions and gives back a result based on the answers.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CheckBox checkBoxOne, checkBoxTwo, checkBoxThree;
+    RadioButton radioButtonOne, radioButtonTwo, radioButtonThree;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Initiate views for the answer toasts.
+        checkBoxOne = findViewById(R.id.check_box_answer_one);
+        checkBoxOne.setOnClickListener(this);
+        checkBoxTwo = findViewById(R.id.check_box_answer_two);
+        checkBoxTwo.setOnClickListener(this);
+        checkBoxThree = findViewById(R.id.check_box_answer_three);
+        checkBoxThree.setOnClickListener(this);
+        radioButtonOne = findViewById(R.id.radio_answer_one);
+        radioButtonOne.setOnClickListener(this);
+        radioButtonTwo = findViewById(R.id.radio_answer_two);
+        radioButtonTwo.setOnClickListener(this);
+        radioButtonThree = findViewById(R.id.radio_answer_three);
+        radioButtonThree.setOnClickListener(this);
+    }
+
+    /**
+     * This sets the click event on buttons and displays toast messages on the selected answers.
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.check_box_answer_one:
+                if (checkBoxOne.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            R.string.toast_check_box_answer_one, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.check_box_answer_two:
+                if (checkBoxTwo.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            R.string.toast_check_box_answer_two, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.check_box_answer_three:
+                if (checkBoxThree.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            R.string.toast_check_box_answer_three, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radio_answer_one:
+                if (radioButtonOne.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            R.string.toast_radio_answer_one, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radio_answer_two:
+                if (radioButtonTwo.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            R.string.toast_radio_answer_two, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radio_answer_three:
+                if (radioButtonThree.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            R.string.toast_radio_answer_three, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    /**
+     * This method gets the rights answers and displays a toast with the results.
+     */
+    public void submitResult(View view) {
+        CheckBox checkBoxAnswerOne = findViewById(R.id.check_box_answer_one);
+        boolean checkBoxAnswerFirst = checkBoxAnswerOne.isChecked();
+
+        CheckBox checkBoxAnswerTwo = findViewById(R.id.check_box_answer_two);
+        boolean checkBoxAnswerSecond = checkBoxAnswerTwo.isChecked();
+
+        CheckBox checkBoxAnswerThree = findViewById(R.id.check_box_answer_three);
+        boolean checkBoxAnswerThird = checkBoxAnswerThree.isChecked();
+
+        RadioButton radioAnswerTwo = findViewById(R.id.radio_answer_two);
+        boolean radioButtonAnswerSecond = radioAnswerTwo.isChecked();
+
+        EditText editTextAnswerOne = findViewById(R.id.edit_text_answer_one);
+        String editTextStringAnswerOne = String.valueOf(editTextAnswerOne.getText());
+        boolean editTextAnswerFirst = false;
+        if (editTextStringAnswerOne.equals("2008")) {
+            editTextAnswerFirst = true;
+        }
+
+        EditText editTextAnswerTwo = findViewById(R.id.edit_text_answer_two);
+        String editTextStringAnswerTwo = String.valueOf(editTextAnswerTwo.getText());
+        boolean editTextAnswerSecond = false;
+        if (editTextStringAnswerTwo.equals("23")) {
+            editTextAnswerSecond = true;
+        }
+
+        int result = calculateResult(
+                checkBoxAnswerFirst,
+                checkBoxAnswerSecond,
+                checkBoxAnswerThird,
+                radioButtonAnswerSecond,
+                editTextAnswerFirst,
+                editTextAnswerSecond);
+
+        displayResult(result);
+    }
+
+    /**
+     * This method checks the right answers and makes a score.
+     *
+     * @param checkBoxAnswerOne    is whether or not selected.
+     * @param checkBoxAnswerTwo    is whether or not selected.
+     * @param checkBoxAnswerThree  is whether or not selected.
+     * @param radioAnswerTwo       is whether or not selected.
+     * @param editTextAnswerOne    is whether or not true.
+     * @param editTextAnswerTwo    is whether or not true.
+     * @return                     the total result of the quiz.
+     */
+    public int calculateResult(boolean checkBoxAnswerOne,
+                               boolean checkBoxAnswerTwo,
+                               boolean checkBoxAnswerThree,
+                               boolean radioAnswerTwo,
+                               boolean editTextAnswerOne,
+                               boolean editTextAnswerTwo) {
+        int result = 0;
+
+        if (checkBoxAnswerOne && checkBoxAnswerTwo && !checkBoxAnswerThree) {
+            result++;
+        }
+        if(radioAnswerTwo) {
+            result++;
+        }
+        if(editTextAnswerOne) {
+            result++;
+        }
+        if(editTextAnswerTwo) {
+            result++;
+        }
+
+        return result;
+    }
+
+    /**
+     * This method displays the total result in a toast message.
+     *
+     * @param result    the total number of right answers.
+     */
+    public void displayResult(int result) {
+        if (result == 0) {
+            Toast.makeText(this, getString(R.string.toast_zero_points_first) +
+                    result +
+                    getString(R.string.toast_zero_points_second), Toast.LENGTH_LONG).show();
+        } else if (result == 1) {
+            Toast.makeText(this, getString(R.string.toast_one_point_first) +
+                    result +
+                    getString(R.string.toast_one_point_second), Toast.LENGTH_LONG).show();
+        } else if (result == 2) {
+            Toast.makeText(this, getString(R.string.toast_two_points) +
+                    result +
+                    "!", Toast.LENGTH_LONG).show();
+        } else if (result == 3) {
+            Toast.makeText(this, result +
+                    getString(R.string.toast_three_points), Toast.LENGTH_LONG).show();
+        } else if (result == 4) {
+            Toast.makeText(this, getString(R.string.toast_four_points_first) +
+                    result +
+                    getString(R.string.toast_four_points_second), Toast.LENGTH_LONG).show();
+        }
+    }
+}
